a528b6bd783011303d1774ea721a8e89b181a70d
riturajnagar <riturajnagar78@gmail.com>
2022-06-15 12:41:23 +0530

    mens page

---
+++ Tees-Polos.js
@@ -0,0 +1,203 @@
+let teesPoloProductsBoysArr=[
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010961226-Green-OLIVEGREEN-1000010961226-18042022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Printed Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010961523-Yellow-YELLOW-1000010961523-07042022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Typographic Print Crew Neck T-shirt"
+},
+
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010961568-Blue-BLUE-1000010961568-10032022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Printed Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010986227-White-WHITE-1000010986227-21052022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Typographic Print Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010986164-Orange-ORANGE-1000010986164-18042022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Typographic Print Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010986213-Blue-AQUA-1000010986213-18042022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Printed Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010961226-Green-OLIVEGREEN-1000010961226-18042022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Printed Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010961523-Yellow-YELLOW-1000010961523-07042022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Typographic Print Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010961190-Black-BLACK-1000010961190-31032022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Typographic Print Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010961559-Red-RED-1000010961559-07042022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Typographic Print Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010690362-Blue-NAVY-1000010690362-2022022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Printed Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010986220-Yellow-YELLOW-1000010986220-07032022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Printed Sleeveless T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010961181-Yellow-YELLOW-1000010961181-18042022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Printed Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010986206-Pink-SALMONPINK-1000010986206-18042022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Printed Sleeveless T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010961523-Yellow-YELLOW-1000010961523-07042022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Typographic Print Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010961190-Black-BLACK-1000010961190-31032022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Typographic Print Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010961208-Orange-ORANGE-1000010961208-10032022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Printed Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000011344012-White-WHITE-1000011344012-05062022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Printed Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010961226-Green-OLIVEGREEN-1000010961226-18042022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Printed Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010961523-Yellow-YELLOW-1000010961523-07042022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Typographic Print Crew Neck T-shirt"
+},
+
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010961568-Blue-BLUE-1000010961568-10032022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Printed Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010986227-White-WHITE-1000010986227-21052022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Typographic Print Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010986164-Orange-ORANGE-1000010986164-18042022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Typographic Print Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010986213-Blue-AQUA-1000010986213-18042022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Printed Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010961226-Green-OLIVEGREEN-1000010961226-18042022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Printed Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010961523-Yellow-YELLOW-1000010961523-07042022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Typographic Print Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010961190-Black-BLACK-1000010961190-31032022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Typographic Print Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010961559-Red-RED-1000010961559-07042022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Typographic Print Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010690362-Blue-NAVY-1000010690362-2022022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Printed Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010986220-Yellow-YELLOW-1000010986220-07032022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Printed Sleeveless T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010961181-Yellow-YELLOW-1000010961181-18042022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Printed Crew Neck T-shirt"
+},
+{
+    image:"https://lmsin.net/cdn-cgi/image/h=1125,w=1125,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/max/1000010986206-Pink-SALMONPINK-1000010986206-18042022_01-2100.jpg",
+    price:169,
+    name:"MAX Boys Printed Sleeveless T-shirt"
+},
+]
+
+displayProducts(teesPoloProductsBoysArr);
+
+function displayProducts(teesPoloProductsBoysArr){
+   document.querySelector("#boysProduct").innerHTML=""
+    teesPoloProductsBoysArr.forEach(function(ele){
+        let div1=document.createElement("div");
+
+        let img=document.createElement("img");
+        let avatar=ele.image
+        img.setAttribute("src",avatar)
+        console.log(img)
+        let price=document.createElement("h5");
+        price.innerText=ele.price
+        let name=document.createElement("p")
+        name.innerText=ele.name;
+
+        let cart=document.createElement("button")
+        cart.innerText="Add to Basket"
+        cart.addEventListener("click",function(){
+            addToBasket(ele);
+        })
+
+        div1.append(img,price,name,cart);
+        document.querySelector("#boysProduct").append(div1);
+    });
+
+    // This code is meant to add items in our cart page
+
+    let basketArr=JSON.parse(localStorage.getItem("basketItem")) || [];
+    function addToBasket(ele){
+        basketArr.push(ele);
+        alert("Added to Basket");
+        localStorage.setItem("basketItem",JSON.stringify(basketArr));
+    }
+
+    
+
+}
